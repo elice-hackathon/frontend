1ebd368a915c71d0cc0cd00e7d9d85eeef6b930b
busankimchi <jacky185@kaist.ac.kr>
2024-11-23 21:54:15 +0900

    feat(main): testing request for production

--- lib/chatApi.ts
+++ lib/chatApi.ts
@@ -1,9 +1,17 @@
-import { Client, ThreadState } from "@langchain/langgraph-sdk";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ThreadState, Client } from "@langchain/langgraph-sdk";
 import { LangChainMessage } from "@assistant-ui/react-langgraph";
 
 const createClient = () => {
-  const apiUrl = process.env["NEXT_PUBLIC_LANGGRAPH_API_URL"] || "/api";
+  const apiUrl =
+    process.env["NEXT_PUBLIC_LANGGRAPH_API_URL"] ||
+    new URL("/api", window.location.href).href;
   return new Client({ apiUrl });
+};
+
+export const createAssistant = async (graphId: string) => {
+  const client = createClient();
+  return client.assistants.create({ graphId });
 };
 
 export const createThread = async () => {
@@ -13,9 +21,23 @@ export const createThread = async () => {
 
 export const getThreadState = async (
   threadId: string
-): Promise<ThreadState<{ messages: LangChainMessage[] }>> => {
+): Promise<ThreadState<Record<string, any>>> => {
   const client = createClient();
   return client.threads.getState(threadId);
+};
+
+export const updateState = async (
+  threadId: string,
+  fields: {
+    newState: Record<string, any>;
+    asNode?: string;
+  }
+) => {
+  const client = createClient();
+  return client.threads.updateState(threadId, {
+    values: fields.newState,
+    asNode: fields.asNode!,
+  });
 };
 
 export const sendMessage = async (params: {
@@ -23,11 +45,16 @@ export const sendMessage = async (params: {
   messages: LangChainMessage[];
 }) => {
   const client = createClient();
+
+  const input: Record<string, any> | null = {
+    messages: params.messages,
+  };
+
   return client.runs.stream(
     params.threadId,
     process.env["NEXT_PUBLIC_LANGGRAPH_ASSISTANT_ID"]!,
     {
-      input: { messages: params.messages },
+      input,
       streamMode: "messages",
     }
   );
